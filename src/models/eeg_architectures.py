d054eebaf21a569fb7608e3f5b616489aae65adf
Nona Rajabi <x_nonra@berzelius1.nsc.liu.se>
2024-05-27 21:54:45 +0200

    Added Some EEG Encoders (To Be Completed)

--- src/models/eeg_architectures.py
+++ src/models/eeg_architectures.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 
 class EEGNet(nn.Module):
-    def __init__(self, n_samples, n_classes, n_channels=32, f1=8, d=2, f2=16, kernel_length=64, dropout_rate=0.5):
+    def __init__(self, n_channels=32, f1=8, d=2, f2=16, kernel_length=64, dropout_rate=0.5):
         super().__init__()
 
         # implementation of the original EEGNet without the classification layer
@@ -29,7 +29,7 @@ class EEGNet(nn.Module):
             ('pool2', nn.AvgPool2d(kernel_size=(1, 8))),
             ('do2', nn.Dropout(p=dropout_rate)),
             ('flat', nn.Flatten()),
-            ('lnout', nn.Linear(f2 * (n_samples // 32), n_classes if n_classes > 2 else 1))
+            # ('lnout', nn.Linear(f2 * (n_samples // 32), n_classes if n_classes > 2 else 1))
         ]))
 
     def forward(self, x):
@@ -37,7 +37,7 @@ class EEGNet(nn.Module):
         return x.unsqueeze(1)
     
 class EEGNet1D(nn.Module):
-    def __init__(self, n_samples, n_classes, n_channels=96, f1=8, d=2, f2=16, kernel_length=64, dropout_rate=0.5):
+    def __init__(self, n_channels=96, f1=8, d=2, f2=16, kernel_length=64, dropout_rate=0.5):
         super().__init__()
         self.model = torch.nn.Sequential(OrderedDict([
             # Input shape: (batch_size, n_channels, n_time_samples)
@@ -56,7 +56,7 @@ class EEGNet1D(nn.Module):
             ('do2', nn.Dropout(p=dropout_rate)),
             ('flat', nn.Flatten()),
             # Adjusted output linear layer size according to your pooling and convolution operations
-            ('lnout', nn.Linear(f2 * ((n_samples // 4) // 8), n_classes if n_classes > 2 else 1))
+            # ('lnout', nn.Linear(f2 * ((n_samples // 4) // 8), n_classes if n_classes > 2 else 1))
         ]))
     def forward(self, x):
         # input shape: (n_trials, n_channels, n_samples)
@@ -64,6 +64,28 @@ class EEGNet1D(nn.Module):
         x = self.model(x)
         return x
 
+
+class lstm(nn.Module):
+    def __init__(self, input_size=128, lstm_size=128, lstm_layers=1, device='cuda'):
+        # Call parent
+        super().__init__()
+        # Define parameters
+        self.input_size = input_size
+        self.lstm_size = lstm_size
+        self.lstm_layers = lstm_layers
+
+        # Define internal modules
+        self.lstm = nn.LSTM(input_size, lstm_size, num_layers=lstm_layers, batch_first=True)
+        self.device = device
+    def forward(self, x):
+        # Forward LSTM and get final state
+        h_0 = torch.randn(self.lstm_layers, x.shape[0], self.lstm_size).to(self.device)
+        c_0 = torch.randn(self.lstm_layers, x.shape[0], self.lstm_size).to(self.device)
+        x = x.to(self.device)
+        x = x.squeeze(dim=1).permute((0, 2, 1))
+        x, (hn, cn) = self.lstm(x, (h_0, c_0))
+
+        return hn[-1, ...]
 
 # Code from GitHub repository of: Lima, E.M., Ribeiro, A.H., Paixão, G.M.M. et al. Deep neural network-estimated electrocardiographic age as a 
 # mortality predictor. Nat Commun 12, 5117 (2021). https://doi.org/10.1038/s41467-021-25351-7. 
@@ -221,5 +243,159 @@ class ResNet1d(nn.Module):
         x = x.view(x.size(0), -1)
 
         # Fully conected layer
-        x = self.lin(x)
+        # x = self.lin(x)
         return x
+    
+# The model proposed by the work: S. Palazzo, C. Spampinato, I. Kavasidis, D. Giordano, J. Schmidt, M. Shah,
+# Decoding Brain Representations by Multimodal Learning of Neural Activity and Visual Features,
+# IEEE TRANSACTIONS ON PATTERN ANALYSIS AND MACHINE INTELLIGENCE, 2020, doi: 10.1109/TPAMI.2
+class ConvLayer2D(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel, stride, padding, dilation):
+        super().__init__()
+        self.add_module('norm', nn.BatchNorm2d(in_channels))
+        self.add_module('relu', nn.ReLU(True))
+        self.add_module('conv', nn.Conv2d(in_channels, out_channels, kernel_size=kernel,
+                                          stride=stride, padding=padding, dilation=dilation, bias=True))
+        self.add_module('drop', nn.Dropout2d(0.2))
+
+    def forward(self, x):
+        return super().forward(x)
+
+
+class TemporalBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, n_layers, kernel_size, stride, dilation_list, in_size):
+        super().__init__()
+        if len(dilation_list) < n_layers:
+            dilation_list = dilation_list + [dilation_list[-1]] * (n_layers - len(dilation_list))
+
+        padding = []
+        # Compute padding for each temporal layer to have a fixed size output
+        # Output size is controlled by striding to be 1 / 'striding' of the original size
+        for dilation in dilation_list:
+            filter_size = kernel_size[1] * dilation[1] - 1
+            temp_pad = math.floor((filter_size - 1) / 2) - 1 * (dilation[1] // 2 - 1)
+            padding.append((0, temp_pad))
+
+        self.layers = nn.ModuleList([
+            ConvLayer2D(
+                in_channels, out_channels, kernel_size, stride, padding[i], dilation_list[i]
+            ) for i in range(n_layers)
+        ])
+
+    def forward(self, x):
+        features = []
+
+        for layer in self.layers:
+            out = layer(x)
+            features.append(out)
+
+        out = torch.cat(features, 1)
+        return out
+
+
+class SpatialBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, num_spatial_layers, stride, input_height):
+        super().__init__()
+
+        kernel_list = []
+        for i in range(num_spatial_layers):
+            kernel_list.append(((input_height // (i + 1)), 1))
+
+        padding = []
+        for kernel in kernel_list:
+            temp_pad = math.floor((kernel[0] - 1) / 2)  # - 1 * (kernel[1] // 2 - 1)
+            padding.append((temp_pad, 0))
+
+        feature_height = input_height // stride[0]
+
+        self.layers = nn.ModuleList([
+            ConvLayer2D(
+                in_channels, out_channels, kernel_list[i], stride, padding[i], 1
+            ) for i in range(num_spatial_layers)
+        ])
+
+    def forward(self, x):
+        features = []
+
+        for layer in self.layers:
+            out = layer(x)
+            features.append(out)
+
+        out = torch.cat(features, 1)
+
+        return out
+
+
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                     stride=stride, padding=1, bias=False)
+
+
+# Residual block
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+class EEGChannelNet(nn.Module):
+    def __init__(self, in_channels, temp_channels, out_channels, input_width, in_height,
+                 temporal_kernel, temporal_stride, temporal_dilation_list, num_temporal_layers,
+                 num_spatial_layers, spatial_stride, num_residual_blocks, down_kernel, down_stride):
+        super().__init__()
+
+        self.temporal_block = TemporalBlock(
+            in_channels, temp_channels, num_temporal_layers, temporal_kernel, temporal_stride, temporal_dilation_list,
+            input_width
+        )
+
+        self.spatial_block = SpatialBlock(
+            temp_channels * num_temporal_layers, out_channels, num_spatial_layers, spatial_stride, in_height
+        )
+
+        self.res_blocks = nn.ModuleList([
+            nn.Sequential(
+                ResidualBlock(
+                    out_channels * num_spatial_layers, out_channels * num_spatial_layers
+                ),
+                ConvLayer2D(
+                    out_channels * num_spatial_layers, out_channels * num_spatial_layers, down_kernel, down_stride, 0, 1
+                )
+            ) for i in range(num_residual_blocks)
+        ])
+
+        self.final_conv = ConvLayer2D(
+            out_channels * num_spatial_layers, out_channels, down_kernel, 1, 0, 1
+        )
+
+    def forward(self, x):
+        out = self.temporal_block(x)
+
+        out = self.spatial_block(out)
+
+        if len(self.res_blocks) > 0:
+            for res_block in self.res_blocks:
+                out = res_block(out)
+
+        out = self.final_conv(out)
+        out = out.view(x.size(0), -1)
+
+        return out
---
+++ src/models/eeg_encoder.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torchvision
+from collections import OrderedDict
+from torchvision.models.feature_extraction import create_feature_extractor, get_graph_node_names
+from src.models import eeg_architectures
+
+
+class EEGEncoder(nn.Module):
+    def __init__(self,
+                 embed_dim=1024,
+                 backbone="eegnet",
+                 n_channels: int = 96,
+                 n_samples: int = 512,
+                 n_classes: int = 40,
+                 **kwargs
+                 ):
+        super(EEGEncoder, self).__init__()
+
+        device = kwargs["device"]
+
+        self.n_channels = n_channels
+        self.n_samples = n_samples
+        self.backbone_type = backbone
+
+        dropout_rate = 0.5
+        kernel_length = 64
+        f1 = 8
+        d = 2
+        f2 = 16
+
+        if backbone == 'eegnet':
+            self.eeg_backbone = eeg_architectures.EEGNet(n_channels=n_channels)
+
+            print(get_graph_node_names(self.eeg_backbone))
+        elif backbone == 'eegnet1d':
+            self.eeg_backbone = eeg_architectures.EEGNet1D(n_channels=n_channels)
+
+        elif backbone == 'EEGChannelNet':
+            # out_channels = 32 for the sake of memory and 50 default, num_residual_blocks = 3 for Ahmed
+            # and 4 for Spampinato
+            self.eeg_backbone = eeg_architectures.EEGChannelNet(in_channels=1, temp_channels=10, out_channels=32,
+                                                                input_width=n_samples, in_height=n_channels,
+                                                                temporal_kernel=(1, 33), temporal_stride=(1, 2),
+                                                                temporal_dilation_list=[(1, 1), (1, 2), (1, 4), (1, 8),
+                                                                                        (1, 16)],
+                                                                num_temporal_layers=4,
+                                                                num_spatial_layers=4, spatial_stride=(2, 1),
+                                                                num_residual_blocks=3, down_kernel=3, down_stride=2)
+            print(get_graph_node_names(self.eeg_backbone))
+
+        elif backbone == 'lstm':
+            self.eeg_backbone = eeg_architectures.lstm(input_size=n_channels, lstm_size=kwargs['lstm_size'],
+                                           lstm_layers=kwargs['lstm_layers'], device=device).to(device)
+            # print(get_graph_node_names(self.eeg_backbone))
+        elif backbone == 'resnet1d':
+            self.eeg_backbone = eeg_architectures.ResNet1d(
+                n_channels=n_channels, 
+                n_samples=n_samples, 
+                net_filter_size=[16, 16, 32, 32, 64], 
+                net_seq_length=[n_samples, 128, 64, 32, 16], 
+                n_classes=n_classes)
+        else:
+            raise NotImplementedError
+
+        self.feature_dim = self.eeg_backbone(
+            torch.zeros(1, 1, n_channels, n_samples)).contiguous().view(-1).size()[0]
+        self.repr_layer = torch.nn.Linear(self.feature_dim, embed_dim)
+
+    def forward(self, x):
+    
+        out = self.eeg_backbone(x)
+        out = self.repr_layer(out)
+        eeg_repr = out
+        
+        return out.squeeze(dim=1), eeg_repr
+
+
+if __name__ == "__main__":
+    model = EEGEncoder(backbone="eegnet", device="cuda:0" if torch.cuda.is_available() else "cpu")
+    x_in = torch.randn((1, 1, 96, 512))
+    x_out = model(x_in)
